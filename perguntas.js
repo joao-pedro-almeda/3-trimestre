85c2f5ff17aca0dc6057c66c18ebbb595a6bc90b
joao-pedro-almeda <161710637+joao-pedro-almeda@users.noreply.github.com>
2024-09-25 08:05:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'Oque é javascript?',
+    'O javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da frança é paris.'
+)
+
+criaCartao(
+    'Programação 2.0',
+    'O que é uma função',
+    'Uma função é um bloco de codigo que executa alguma tarefa'
+)
